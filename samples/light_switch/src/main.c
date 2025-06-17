f8ddc7cd3b908ffee897490cfc1907976a52a52c
Adam Zelik <adam.zelik@nordicsemi.no>
2025-06-17 14:07:08 +0200

    [DNM] Change light_switch start message

--- samples/light_switch/src/main.c
+++ samples/light_switch/src/main.c
@@ -811,7 +811,7 @@ void set_tx_power(void)
 
 int main(void)
 {
-	LOG_INF("Starting Zigbee R23 Light Switch example");
+	LOG_INF("Starting Zigbee R23 Light Switch (54L-fota) example");
 
 	/* Initialize. */
 	configure_gpio();
