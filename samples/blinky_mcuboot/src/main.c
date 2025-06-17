98e4e1fdb98922a48be4b7fc98894f1253f50508
Adam Zelik <adam.zelik@nordicsemi.no>
2025-06-17 18:29:39 +0200

    Fix blinky_mcuboot/main

--- samples/blinky_mcuboot/src/main.c
+++ samples/blinky_mcuboot/src/main.c
@@ -31,26 +31,28 @@
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
-void main(void)
+int main(void)
 {
 	int ret;
 
 	printk("Hello world from %s\n", CONFIG_BOARD);
 
 	if (!device_is_ready(led.port)) {
-		return;
+		return 0;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
-		return;
+		return 0;
 	}
 
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
-			return;
+			return 0;
 		}
 		k_msleep(SLEEP_TIME_MS);
 	}
+    
+    return 0;
 }
