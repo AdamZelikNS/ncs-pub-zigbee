2eaa07ba97bc6115c27477393bb941402ee20c5e
Eduardo Montoya <eduardo.montoya@nordicsemi.no>
2025-07-14 13:50:08 +0200

    lib: update ZBOSS libraries
    - Update ZBOSS to v4.2.2.0.
    
    Signed-off-by: Eduardo Montoya <eduardo.montoya@nordicsemi.no>

--- lib/zboss/include/osif/build_info.h
+++ lib/zboss/include/osif/build_info.h
@@ -11,9 +11,9 @@
  * Please use release scripts to update it's contents.
  *
  * Repositories:
- * platform_ncs       391829ac38b02b52c5d2b7ae49b1dee480fe3ac1 https://projecttools.nordicsemi.no/bitbucket/scm/zoi/platform_ncs.git
- * zboss-stack-src    48d7622eb0265c84d035a9bd8e7aca45e944701b https://projecttools.nordicsemi.no/bitbucket/scm/zoi/zboss_r23
- * ncs-zigbee         06fcd30fae95b9d640e46061df37989389204f37 https://github.com/nrfconnect/ncs-zigbee
+ * platform_ncs       7c31bcc05f50c65a13c147c6a95dd9261ec603ec https://projecttools.nordicsemi.no/bitbucket/scm/zoi/platform_ncs.git
+ * zboss-stack-src    d35e796b8573f8883fd42bfbe900bb9b9a5dca4f https://projecttools.nordicsemi.no/bitbucket/scm/zoi/zboss_r23
+ * ncs-zigbee         ca9add06d4c48557b8775a2d25bc9864e9f178a2 https://github.com/edmont/ncs-zigbee
  * nrf                7787b264984022cda64d9629278942053e6462a5 https://github.com/nrfconnect/sdk-nrf
  * zephyr             1f8f3dc291420c70cd39e77a5cdc954561d4a08f https://github.com/nrfconnect/sdk-zephyr
  * wfa-qt-control-app af011c8a8d338ba529f17aed2cc2ef4c1c591a58 https://github.com/nrfconnect/sdk-wi-fiquicktrack-controlappc
@@ -70,16 +70,16 @@
 
 
 /** ZBOSS build tag */
-#define ZBOSS_BUILD_TAG "ncs_release-v4.2.2.1"
+#define ZBOSS_BUILD_TAG "d35e796b8"
 
 /** ZBOSS platform build tag */
-#define ZBOSS_PLATFORM_BUILD_TAG "v4.2.2.0+v6.0.6"
+#define ZBOSS_PLATFORM_BUILD_TAG "v4.1.14.2122+v6.0.4"
 
 /** ZBOSS build date (UTC) */
-#define ZBOSS_BUILD_DATE 20250807
+#define ZBOSS_BUILD_DATE 20250714
 
 /** ZBOSS build time (UTC) */
-#define ZBOSS_BUILD_TIME 091605
+#define ZBOSS_BUILD_TIME 114202
 
 
 #endif /* ZBOSS_BUILD_INFO_H__ */
